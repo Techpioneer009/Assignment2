4bb1cda6ca8f4a91aaef985f73a55bfcdc141ecc
Techpioneer009 <kkush5379@gmail.com>
2025-09-24 20:17:24 +0530

    Delete Assignment2/PartA directory

--- Assignment2/PartA/Add.js
+++
@@ -1,6 +0,0 @@
-let number1 = 15;
-let number2 = 25;
-
-let Result = number1 + number2;
-
-console.log("The sum is: " + Result);
--- Assignment2/PartA/Even.js
+++
@@ -1,7 +0,0 @@
-let number = 24;
-
-if (number % 2 === 0) {
-    return `${number} is an Even number.`;
-} else {
-    return `${number} is an Odd number.`;
-}
