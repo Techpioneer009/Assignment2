48b221c140af1f944785a3ffb9293f79fa3c3f0c
Techpioneer009 <kkush5379@gmail.com>
2025-09-24 19:43:38 +0530

    Add files via upload

---
+++ Assignment2/PartA/Add.js
@@ -0,0 +1,6 @@
+let number1 = 15;
+let number2 = 25;
+
+let Result = number1 + number2;
+
+console.log("The sum is: " + Result);
---
+++ Assignment2/PartA/Even.js
@@ -0,0 +1,7 @@
+let number = 24;
+
+if (number % 2 === 0) {
+    return `${number} is an Even number.`;
+} else {
+    return `${number} is an Odd number.`;
+}
